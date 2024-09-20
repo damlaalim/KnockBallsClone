ad560a9432aca4462ebc61e367c808121d10589c
damlaalim <damlaalim81@gmail.com>
2024-09-20 15:50:39 +0300

    added tnt particle

--- Assets/_knockBalls/Scripts/Target/TntTargetController.cs
+++ Assets/_knockBalls/Scripts/Target/TntTargetController.cs
@@ -5,8 +5,13 @@ namespace _knockBalls.Scripts.Target
     public class TntTargetController : TargetController
     {
         [SerializeField] private float _radius = 5f, _power = 100f, _upForce = 1f;
+        [SerializeField] private ParticleSystem _explodeParticle;
+        
         public void Explode()
         {
+            var particle = Instantiate(_explodeParticle);
+            particle.Play();
+            
             var explosionPos = transform.position;
             var colliders = Physics.OverlapSphere(explosionPos, _radius);
             
