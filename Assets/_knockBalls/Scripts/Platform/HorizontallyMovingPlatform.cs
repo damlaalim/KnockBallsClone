1b4225f80b865058282e63413cb52a32eff3a04a
damlaalim <damlaalim81@gmail.com>
2024-09-20 15:31:38 +0300

    added moving platforms

---
+++ Assets/_knockBalls/Scripts/Platform/HorizontallyMovingPlatform.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace _knockBalls.Scripts.Platform
+{
+    public class HorizontallyMovingPlatform : MonoBehaviour
+    {
+        public float speed = 2f; 
+        public float leftLimit = -5f;
+        public float rightLimit = 5f;
+
+        void Update()
+        {
+            var xPosition = Mathf.PingPong(Time.time * speed, rightLimit - leftLimit) + leftLimit;
+
+            transform.position = new Vector3(xPosition, transform.position.y, transform.position.z);
+        }
+    }
+}
---
+++ Assets/_knockBalls/Scripts/Platform/RotatingPlatform.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace _knockBalls.Scripts.Platform
+{
+    public class RotatingPlatform : MonoBehaviour
+    {
+        [SerializeField] private Vector3 _rotation;
+
+        private void Update()
+        {
+            transform.Rotate(_rotation * Time.deltaTime);
+        }
+    }
+}
